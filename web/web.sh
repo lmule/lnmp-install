7fbc71d5413366c40bba5f1c8f4e4de1930e163d
dingrui <dingrui198424@126.com>
2016-07-26 12:54:10 +0800

    fix

--- web/web.sh
+++ web/web.sh
@@ -5,7 +5,8 @@ cp web/conf/yii.conf $web_conf
 sed -i "s#{root_path}#${default_web_dir}${server_name}#" $web_conf
 sed -i "s#{log_path}#${default_log_dir}${nginx_prefix}#" $web_conf
 sed -i "s#{server_name}#${server_name}#" $web_conf
-sed -i "s#{server_port}#${server_port}#" $web_conf
+sed -i "s#{server_frontend_port}#${server_frontend_port}#" $web_conf
+sed -i "s#{server_backend_port}#${server_backend_port}#" $web_conf
 service nginx restart
 
 cd ${current_dir}
